bef17c2fcc82978b2149a5ada244f96140b7e860
ARahmaan96 <a.rahman.khallaf@gmail.com>
2024-07-07 15:41:44 +0300

    update the fetchRepositories return type.

--- github_task/src/api/repository.ts
+++ github_task/src/api/repository.ts
@@ -5,15 +5,25 @@ interface IPrams {
   keyword?: String;
 }
 
-const fetchRepositories = async ({perPage = 10, keyword = ''}: IPrams) => {
+interface SearchResponse {
+  total_count: number;
+  incomplete_results: boolean;
+  items: Repository[];
+}
+
+const fetchRepositories = async ({
+  perPage = 10,
+  keyword = '',
+}: IPrams): Promise<Repository[]> => {
   try {
-    const response = await api.get(
+    const response = await api.get<SearchResponse>(
       `/search/repositories?q=${keyword}&sort=stars&order=desc&per_page=${perPage}`,
     );
     return response.data.items;
   } catch (error) {
     // TODO: Log error to monitoring services
     console.log('error read data:', error);
+    return [];
   }
 };
 
